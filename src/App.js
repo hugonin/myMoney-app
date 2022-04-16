9c54c03768696b889a96c3d3648021d6057effd5
Hugonin <jeanhugues.doumbe@gmail.com>
2022-04-16 19:36:20 +0200

    feat: Added Firebase Auth State Changes and Route guarding

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from "./pages/home/Home"
-import Login from "./pages/login/Login"
-import SignUp from "./pages/signup/SignUp"
-import Navbar from './components/navbar/Navbar';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useAuth } from "./hooks/useAuth";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import SignUp from "./pages/signup/SignUp";
+import Navbar from "./components/navbar/Navbar";
 
-
-import './App.css';
+import "./App.css";
 
 function App() {
+  const { authIsReady, user } = useAuth();
+
   return (
     <div className="App">
-    <BrowserRouter>
-    <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-      </Routes>
-    </BrowserRouter>
-    
+      {authIsReady && (
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route
+              path="/"
+              element={user ? <Home /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/login"
+              element={user ? <Navigate to="/" /> : <Login />}
+            />
+            <Route
+              path="/signup"
+              element={user ? <Navigate to="/" /> : <SignUp />}
+            />
+          </Routes>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,5 @@
-import { createContext, useReducer } from "react";
+import { createContext, useEffect, useReducer } from "react";
+import { projectAuth } from "../firebase/config";
 
 export const AuthContext = createContext();
 
@@ -8,6 +9,8 @@ const authReducer = (state, action) => {
       return { ...state, user: action.payload };
     case "LOG_OUT":
       return { ...state, user: null };
+    case "AUTH_IS_READY" :
+      return { ...state, user: action.payload, authIsReady: true} 
     default:
       return state;
   }
@@ -16,7 +19,16 @@ const authReducer = (state, action) => {
 export function AuthProvider({ children }) {
   const [state, dispatch] = useReducer(authReducer, {
     user: null,
+    authIsReady: false,
   });
+
+  useEffect(() => {
+    const unsub = projectAuth.onAuthStateChanged((user) => {
+      dispatch({ type: 'AUTH_IS_READY', payload: user})
+      unsub()
+
+    })
+  }, [])
 
   console.log("Auth state:", state);
 
