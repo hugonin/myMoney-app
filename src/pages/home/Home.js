db3a2ee0162a991e2260819e8760a46991b70221
Hugonin <jeanhugues.doumbe@gmail.com>
2022-04-17 15:46:08 +0200

    feat: Made a transaction form, created a useFirestore and useCollection hook, listed transactions and added a case to delete transactions

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,33 @@
-//import styles from "./Home.module.css"
+import { useAuth } from "../../hooks/useAuth";
+import { useCollection } from "../../hooks/useCollection";
+import TransactionForm from "../../components/TransactionForm";
+import TransactionList from "../../components/TransactionList";
 
+import styles from "./Home.module.css";
 
 export default function Home() {
+  const { user } = useAuth();
+  const { documents, error } = useCollection(
+    "transactions", 
+    [
+      "uid",
+      "==",
+      user.uid,
+    ],
+    [
+      "createdAt",
+      "desc"
+    ]
+    );
   return (
-    <div>
-      Home
+    <div className={styles.container}>
+      <div className={styles.content}>
+        {error && <p>{error}</p>}
+        {documents && <TransactionList transactions={documents} />}
+      </div>
+      <div className={styles.sidebar}>
+        <TransactionForm uid={user.uid} />
+      </div>
     </div>
-  )
+  );
 }
